b632810700c83927bebbcb8a314ae3e9032875a9
Debonik Pal <73780823+Debonik@users.noreply.github.com>
2023-06-02 17:54:59 +0530

    Update On Page SEO Analyzer.py

--- On Page SEO Analyzer.py
+++ On Page SEO Analyzer.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = ''  # replace with the URL you want to analyze
+url = '[URL]'  # replace with the URL you want to analyze
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
